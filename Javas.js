f2f5e29b0aa638a10c309f1d082dcfdfbabc09f9
Jay Mark <melivojaymark61@gmail.com>
2025-08-03 02:18:44 +0800

    modified:   Front.home.html 	modified:   Frontcss.css 	new file:   Javas.js 	new file:   Pics/css-3.png 	new file:   Pics/g1.png 	new file:   Pics/g2.jpg 	new file:   Pics/g3.jpg 	new file:   Pics/g4.jpg 	new file:   Pics/g5.jpg 	new file:   Pics/html.png 	new file:   Pics/js.png

---
+++ Javas.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper(".myswiper", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
